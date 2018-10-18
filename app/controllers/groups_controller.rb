baa5ac18b63f890fd654c28d88caf4c06699c339
Luke Cassar <lukecassar7@gmail.com>
2018-10-18 12:38:28 +1000

    Create Group MVC CRUD.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,43 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_path(@group), notice: 'New Group Created'
+    else
+      flash[:alert] = @group.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path(@group), notice: 'New Group Updated'
+    else
+      flash[:alert] = @group.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
