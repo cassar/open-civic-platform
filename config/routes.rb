5c6aa740956a33c909bcc716ebb6504e41c4d7a1
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-07-15 21:12:16 +1000

    Modify devise routes and nav bar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+    get 'account', to: 'devise/registrations#edit'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :memberships, only: :destroy
