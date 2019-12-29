9f947414000cc0a0c449d0f63938cbdae03f1d0e
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-12-29 23:42:04 +1000

    Replace coming soon controller tests with landing page tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'coming soon as root' do
+  test 'landing page as root' do
     get root_url
     assert_response :success
-    assert_select '#logo'
-    assert_select '#message'
+    assert_select 'logo'
+    assert_select '.options'
   end
 end
