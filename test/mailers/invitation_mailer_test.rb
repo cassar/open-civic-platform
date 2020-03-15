9a846d52ce8552dd77015b72447eee3f9f4ad9ff
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-03-15 14:04:15 +1000

    Restyle Invitation mailer spreadin parameters out

--- test/mailers/invitation_mailer_test.rb
+++ test/mailers/invitation_mailer_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class InvitationMailerTest < ActionMailer::TestCase
   test 'invitation_email' do
-    assert InvitationMailer.invitation_email User.second, Profile.second, Profile.first, Group.first
+    assert InvitationMailer.invitation_email(
+      User.second,
+      Profile.second,
+      Profile.first,
+      Group.first
+    ).deliver_now
   end
 end
