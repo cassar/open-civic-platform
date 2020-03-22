607960c01b519e59d3e8863525bb8b9b74aabde4
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-03-22 10:08:08 +1000

    Add tests to invitations controller

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class InvitationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @group = Group.first
+    sign_in users(:one)
+    @invitation = Membership.second
+  end
+
+  test 'should patch update' do
+    assert @invitation.invited?
+    patch invitation_url(@invitation)
+    assert_redirected_to group_path(@group)
+    @invitation.reload
+    assert @invitation.confirmed?
+  end
+
+  test 'should delete destroy atleast' do
+    assert @invitation.invited?
+    assert @group.memberships.confirmed.present?
+    assert_difference 'Membership.count', -1 do
+      delete invitation_url(@invitation)
+    end
+    assert_redirected_to groups_path
+  end
 end
