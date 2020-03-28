5a6a0889e4f2e095810402255b03a0e46177517d
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-03-28 10:56:42 +1000

    Move membership up to match model layout in support test

--- test/models/support_test.rb
+++ test/models/support_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class SupportTest < ActiveSupport::TestCase
   setup do
     @support = Support.first
+    @membership = Membership.first
     @position = Position.first
     @issue = Issue.first
-    @membership = Membership.first
     @profile = Profile.first
     @user = User.first
   end
