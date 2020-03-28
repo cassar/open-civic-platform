4a55509f1c609e6ec9de97a18c3d9cda25367914
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-03-28 11:04:56 +1000

    Add style space in association declarations in issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,6 @@
 class Issue < ApplicationRecord
   belongs_to :group
+
   has_many :positions, dependent: :destroy
   has_many :supports, through: :positions
   has_many :supporter_users, through: :supports, source: :user
