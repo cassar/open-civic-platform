5ba941031dc91253a0549158dc81aff60e57085a
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-03-28 11:01:02 +1000

    Add supporter user association to issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue < ApplicationRecord
   belongs_to :group
   has_many :positions, dependent: :destroy
   has_many :supports, through: :positions
+  has_many :supporter_users, through: :supports, source: :user
 
   validates :name, presence: true
 end
--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -6,12 +6,14 @@ class IssueTest < ActiveSupport::TestCase
     @group = Group.first
     @position = Position.first
     @support = Support.first
+    @user = User.first
   end
 
   test 'issue associations' do
     assert @issue.group == @group
     assert @issue.positions.include? @position
     assert @issue.supports.include? @support
+    assert @issue.supporter_users.include? @user
     @issue.destroy
     assert_raises(ActiveRecord::RecordNotFound) { @position.reload }
   end
