c73898f7e185397030a1d30ea12e6da49a8452e8
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-06-22 15:35:51 +1000

    Add supports and issues supporting associations to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,4 +5,7 @@ class Profile < ApplicationRecord
   has_many :confirmed_memberships, -> { confirmed }, class_name: 'Membership'
   has_many :invitations, -> { invited }, class_name: 'Membership', dependent: :destroy
   has_many :groups, through: :confirmed_memberships
+
+  has_many :supports, dependent: :destroy
+  has_many :issues_supporting, through: :supports, source: :issue
 end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -11,16 +11,21 @@ class ProfileTest < ActiveSupport::TestCase
     @confirmed_membership = @membership
     @invitation = Membership.third
     @group = Group.first
+    @support = supports(:one)
+    @issue = issues(:one)
   end
 
-  test 'profile associations' do
+  test 'associations' do
     assert @profile_user.userable == @user
     assert @profile_placeholder.userable == @placeholder
     assert @profile.memberships.include? @membership
     assert @profile.confirmed_memberships.include? @confirmed_membership
     assert @profile.invitations.include? @invitation
     assert @profile.groups.include? @group
+    assert @profile.supports.include? @support
+    assert @profile.issues_supporting.include? @issue
     @profile.destroy
     assert_raises(ActiveRecord::RecordNotFound) { @membership.reload }
+    assert_raises(ActiveRecord::RecordNotFound) { @support.reload }
   end
 end
