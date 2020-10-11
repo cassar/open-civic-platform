80127ccd58576744f94d1d941766448af680686f
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-10-11 18:28:00 +1000

    Add link thumbnails to issue page

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,6 +6,7 @@ class IssuesController < ApplicationController
   def show
     @issue = Issue.find_by_id params[:id]
     @positions = @issue.positions
+    @thumbnails = @issue.links.map { |link| LinkThumbnailer.generate(link.url) }
     return unless user_signed_in?
 
     @adoption = current_user.adoptions.find_by issue: @issue
