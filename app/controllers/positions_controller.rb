b2b0f40405559dd4bab57ef3584070e14f471ea6
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-10-25 13:29:29 +1000

    Hide links when there are not any or when a user is not signed in

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,6 +2,7 @@ class PositionsController < ApplicationController
   def show
     @issue = Issue.find_by_id params[:issue_id]
     @position = Position.find_by_id params[:id]
+    @links = @position.links
     return unless user_signed_in?
 
     @support = current_user.supports.where_issue(@issue).find_by position: @position
